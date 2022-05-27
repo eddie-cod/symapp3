192b8de933ad333b685d8a320086862738ea9af5
eddie-cod <djihouae@3il.fr>
2022-05-27 05:12:20 +0200

    Test Response

--- public/index.php
+++ public/index.php
@@ -4,20 +4,23 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 use App\Kernel;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-
-// return function (array $context) {
-//     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-// };
+$response = new Response();
 
 $name = $request->get('name');
 $nom = $request->get('nom');
 header('Content-Type: text/html; charset=utf-8');
-echo '<html>';
-var_dump($name);
-var_dump($nom);
 
-echo '<body><h1>Hello World! '.$name.' -> '.$nom.'</h1></body>';
-echo '</html>';
+$response->setContent('<html><body><h1>Hello World! '.$name.' -> '.$nom.'</h1></body></html>');
+$response->send();
 
+
+
+
+
+
+// return function (array $context) {
+//     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+// };
